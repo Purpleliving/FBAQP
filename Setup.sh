02dd66917189bff773ad628a937557b0d9a656ec
Purpleliving <142207135+Purpleliving@users.noreply.github.com>
2024-10-28 22:28:34 +0100

    Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -10,8 +10,8 @@ pkg install jq -y
 pkg install git -y
 
 # Clone your repository (replace 'yourusername' and 'yourrepository' with actual names)
-git clone https://github.com/yourusername/yourrepository.git
-cd yourrepository
+git clone https://github.com/Purpleliving/FBAQP.git
+cd FBAQP
 
 # Make the main script executable
 chmod +x quote.sh
